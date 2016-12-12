5c1194cfe0f4dd240875a84da2e174c5d8a7ee86
evcu <evcu@users.noreply.github.com>
2016-12-11 20:23:15 -0500

    Mnist data addadded

--- project/hpc/main.sh
+++ project/hpc/main.sh
@@ -6,7 +6,9 @@
 #PBS -e localhost:/scratch/ue225/${PBS_JOBNAME}.e${PBS_JOBID}
 #PBS -o localhost:/scratch/ue225/${PBS_JOBNAME}.o${PBS_JOBID}
 
-EPOCHS = 1
+EPOCHS=1
+OUT_FOLDER=$HOME/cv2016/out
+LOG_FOLDER=$SCRATCH/cvproj/
 
 cd $PBS_JOBTMP 
 cp -r $HOME/cv2016/project ./
@@ -14,6 +16,8 @@ cd project
 module load torch/gnu/20160623 
 time qlua main.lua  -nEpochs $EPOCHS -model lenet5 -jobID ${PBS_JOBID}
 zip -r $PBS_JOBID.zip logs
-curl --upload-file $PBS_JOBID.zip https://transfer.sh/$PBS_JOBID.zip > $HOME/output/$PBS_JOBID
-cp $PBS_JOBID.zip $HOME/output/
+curl --upload-file $PBS_JOBID.zip https://transfer.sh/$PBS_JOBID.zip > $OUT_FOLDER/$PBS_JOBID
+cp $PBS_JOBID.zip $OUT_FOLDER
+mv $SCRATCH/${PBS_JOBNAME}.e${PBS_JOBID} $LOG_FOLDER
+mv $SCRATCH/${PBS_JOBNAME}.o${PBS_JOBID} $LOG_FOLDER
 exit 0;
