c8e7b1f46635c31143a7d9f4de2127078b88b90e
evcu <evcu@users.noreply.github.com>
2016-12-13 17:48:05 -0500

    test folder is excluded

--- project/hpc/main.sh
+++ project/hpc/main.sh
@@ -6,7 +6,10 @@
 #PBS -e localhost:/scratch/ue225/${PBS_JOBNAME}.e${PBS_JOBID}
 #PBS -o localhost:/scratch/ue225/${PBS_JOBNAME}.o${PBS_JOBID}
 
-EPOCHS=30
+EPOCHS=3
+NSAMPLE=3
+declare -a LAYERS = (1,4,8,10,12)
+
 OUT_FOLDER=$HOME/cv2016/out
 LOG_FOLDER=$SCRATCH/cvproj/
 
@@ -14,7 +17,14 @@ cd $PBS_JOBTMP
 cp -r $HOME/cv2016/project ./
 cd project
 module load torch/gnu/20160623 
-time qlua main.lua  -nEpochs $EPOCHS -model lenet5 -jobID ${PBS_JOBID}
+
+
+for l in "${LAYERS[@]}"
+do 
+	time qlua main.lua  -nEpochs $EPOCHS -LSP $NSAMPLE -l $l -model lenet5 -jobID ${PBS_JOBID}
+done
+
+
 zip -r $PBS_JOBID.zip logs
 curl --upload-file $PBS_JOBID.zip https://transfer.sh/$PBS_JOBID.zip > $OUT_FOLDER/$PBS_JOBID
 cp $PBS_JOBID.zip $OUT_FOLDER/
