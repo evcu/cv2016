c60890da71e744660e5886bc9b4e683a582b2b1b
evcu <evcu@users.noreply.github.com>
2016-12-21 21:31:22 -0500

    Autopush

--- project/utils/pruner.lua
+++ project/utils/pruner.lua
@@ -57,7 +57,7 @@ end
 function pruner:getConnectionMult(c_w,i)
 	bfun = function(state)
 			print('getConnectionMult')
-			print(state.model:get(i).weight[1])--TODO fix this!
+			print(state.network:get(i).weight[1])--TODO fix this!
 			state.network:get(i).weight:cmul(c_w)
 			if verbose then
 				--print('Layer'..k..': Weight multiplied\n')
