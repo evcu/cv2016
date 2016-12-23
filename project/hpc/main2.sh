8c8aaf0c203f81e2db56285cdf0f917a939ca2d8
evcu <evcu@users.noreply.github.com>
2016-12-22 22:19:14 -0500

    Autopush

---
+++ project/hpc/main2.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+#PBS -l nodes=1:ppn=1:gpus=1,walltime=4:30:00,mem=8GB
+#PBS -N cvTrial1
+#PBS -M ue225@nyu.edu
+#PBS -m abe
+#PBS -e localhost:/scratch/ue225/${PBS_JOBNAME}.e${PBS_JOBID}
+#PBS -o localhost:/scratch/ue225/${PBS_JOBNAME}.o${PBS_JOBID}
+
+EPOCHS=1
+NSAMPLE=40
+
+OUT_FOLDER=$HOME/cv2016/out
+LOG_FOLDER=$SCRATCH/cvproj/
+
+cd $PBS_JOBTMP 
+cp -r $HOME/cv2016/project ./
+cd project
+module load torch/gnu/20160623 
+
+time qlua main.lua  -reTrain -nEpochs $EPOCHS -LSP $NSAMPLE -jobID ${PBS_JOBID}
+
+zip -r $PBS_JOBID.zip logs
+curl --upload-file $PBS_JOBID.zip https://transfer.sh/$PBS_JOBID.zip > $OUT_FOLDER/$PBS_JOBID
+cp $PBS_JOBID.zip $OUT_FOLDER/
+mv $SCRATCH/${PBS_JOBNAME}.e${PBS_JOBID} $LOG_FOLDER
+mv $SCRATCH/${PBS_JOBNAME}.o${PBS_JOBID} $LOG_FOLDER
+exit 0;
