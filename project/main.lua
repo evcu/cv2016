81c8cf12e94951b142216df1470527206d0847f8
evcu <evcu@users.noreply.github.com>
2016-12-23 13:07:14 -0500

    Autopush

--- project/main.lua
+++ project/main.lua
@@ -19,6 +19,8 @@ end
 function isCuda(_item)
     if opt.cuda then
         _item = _item:cuda()
+    else
+        _item = _item:double()
     end
     return _item
 end
@@ -56,8 +58,11 @@ local testDataset = tnt.ListDataset{
     end
 }
 
-
-local model = isCuda(torch.load('inp/'..opt.model..'.t7'))
+if opt.test then
+    local model = isCuda(torch.load('logs/'..opt.model..'.t7pruned'))
+else
+    local model = isCuda(torch.load('inp/'..opt.model..'.t7'))
+end
 local engine = tnt.OptimEngine()
 local meter = tnt.AverageValueMeter()
 local criterion = isCuda(nn.CrossEntropyCriterion())
@@ -138,6 +143,13 @@ end
 
 local init_acc = TestModel(model)
 local pruner = require('utils.pruner')
+if opt.test then
+    isCuda(torch.load('inp/'..opt.model..'.t7'))
+    print('Total Compression: '..pruner:calculateCompression())
+    print('Accuracy: '..init_acc)
+    os.exit()
+end
+
 local prunerFunc = ((opt.pruner =='emp') and pruner.maskEmprical) or ((opt.pruner =='taylor1') and pruner.maskTaylor1) or ((opt.pruner =='taylor2') and pruner.maskTaylor2) or ((opt.pruner =='l1') and pruner.maskL1) or ((opt.pruner =='l2') and pruner.maskL2) or ((opt.pruner =='mag') and pruner.maskPercentage) or nil
 assert(prunerFunc ~= nil, 'Pruner function can\'t set, fix the code')
 pruner:setVariables(model,prunerFunc,TrainModel,TestModel,CalculateHessianValues)
@@ -176,7 +188,9 @@ plot_file:close()
 
 --print(pruner:calculateCompression())
 model:clearState()
-torch.save(opt.logDir ..'/'.. opt.model..'.t7pruned', model)
+torch.save(opt.logDir ..'/'.. opt.model:float()..'.t7pruned.f', model)
+torch.save(opt.logDir ..'/'.. opt.model:double()..'.t7pruned.d', model)
+
 
 
 
